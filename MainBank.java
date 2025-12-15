c984329ec6bb77f286e0c91249bf53cb654d472b
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-12-15 07:58:29 -0500

    Add files via upload

--- MainBank.java
+++ MainBank.java
@@ -2,9 +2,20 @@ package javaPack;
 
 public class MainBank {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+		//object and constructor creation
 		DisplayMain();
 	}
-
+	
+	static void DisplayMain()
+	{
+		//Set the mutator to set the data 
+		BankAccount myAccount = new BankAccount();
+		myAccount.setAccountHolder("Toji");
+		myAccount.setBalance(-10000000);
+		
+		System.out.println("User: " + myAccount.getAccountHolder());
+		System.out.println("Balance: $" + myAccount.getBalance());
+	}
 }
